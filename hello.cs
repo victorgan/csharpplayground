006aea603194163855feb0ca48684b90d27d3ef7
Victor Gan <victorgan@gmail.com>
2015-10-22 15:36:18 -0700

    hello world

---
+++ hello.cs
@@ -0,0 +1,11 @@
+// Following: https://msdn.microsoft.com/en-us/library/aa664628(v=vs.71).aspx
+// compile with csc hello.cs; then run executable in command line
+using System;
+class Hello
+{
+    static void Main()
+    {
+        Console.WriteLine("hello, world");
+        // same as System.Console.WriteLine("hello, world");
+    }
+}
